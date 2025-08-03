3f58d166e3cda3ae77fcb0c2def17addeabe8abe
John Hardy <unatahjohn@gmail.com>
2025-08-03 23:19:13 +0100

    first commit

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,52 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+import  ai from "../../assets/A.I-image.jpg";
+
+export const Home = () => {
+
+  return (
+    <section
+      id="home"
+      className=" min-h-screen flex items-center justify-center relative right-8 left-0 top-0  bg-cover bg-center bg-no-repeat"
+      
+    >
+      <div
+    className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+    style={{
+      backgroundImage: `url(${ai})`,
+      opacity: 0.17,
+      zIndex: 0,
+    }}
+  />
+      <RevealOnScroll>
+        <div className=" text-center z-10 px-4 ">
+          <h1 className="relative text-5xl md:text-7xl font-bold z-50 mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right opacity-100">
+            Hi, I'm John Hardy
+          </h1>
+
+          <p className=" tex-gray-400 text-lg mb-8 max-w-lg mx-auto  z-20">
+            I’m a full-stack developer who loves crafting clean, scalable web
+            applications. My goal is to build solutions that offer both
+            exceptional performance and a delightful user experience.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="#projects"
+              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+            >
+              View Projects
+            </a>
+
+            <a
+              href="#contact"
+              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+            >
+              Contact Me
+            </a>
+          </div>
+        </div>
+        
+      </RevealOnScroll>
+    </section>
+  );
+};
